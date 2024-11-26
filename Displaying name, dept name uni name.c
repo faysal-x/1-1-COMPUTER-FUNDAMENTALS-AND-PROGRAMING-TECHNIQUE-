dba5d2b392b4edb14a76f4ade1113d528785c69d
Faysal Al Shariar <faysalalshariar@gmail.com>
2024-11-27 00:40:46 +0600

    Create Displaying name, dept name uni name.c

---
+++ Displaying name, dept name uni name.c
@@ -0,0 +1,12 @@
+//Faysal 26 Nov 2024 
+
+#include <stdio.h>
+
+int main() {
+    printf("Name: Faysal\n");
+    printf("Dept. Name: EEE\n");
+    printf("Uni Name: PUST\n");
+    
+
+    return 0;
+}
