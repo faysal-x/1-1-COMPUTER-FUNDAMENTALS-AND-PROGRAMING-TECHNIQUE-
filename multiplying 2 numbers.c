d21f4386c5d9a6efcd997a350a8f3fe025d5c296
Faysal Al Shariar <faysalalshariar@gmail.com>
2024-11-27 09:04:07 +0600

    Update

---
+++ multiplying 2 numbers.c
@@ -0,0 +1,15 @@
+//Faysal 26 nov 2024 multiplying 2 numbers
+
+
+ #include <stdio.h>
+
+ int main(){
+   int num1=4;
+   int num2=5;
+   int multiple;
+multiple=num1*num2;
+
+printf("%d", multiple);
+
+     return 0;
+ }
